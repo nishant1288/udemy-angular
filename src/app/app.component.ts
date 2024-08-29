f21ff8d421b3f6db2af5d95aec33d0385f66e7ea
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2024-08-29 05:51:23 +0530

    event key status and keyboard events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'myfirstProject';
 
-  name : string = '';
+  name: string = '';
 
-  textVal : string = 'Angular';
-  isDisabled : boolean = false;
+  textVal: string = 'Angular';
+  isDisabled: boolean = false;
 
   display() {
-    alert('Hello')
+    // alert('Hello')
   }
- 
+
+  onKeyDown() {
+    // alert('On key down')
+  }
+
+  onSelect() {
+    // alert('Selected')
+  }
+
+  onCut() {
+    // alert('Cut event')
+  }
+
+  onInputClick(e: any) {
+    console.log(e.target.value)
+  }
+
+  keyevent() {
+    console.log('Shift + Y is pressed');
+  }
+
+  secondKeyEvent(event: any) {
+    if (event.shiftKey && event.key === 'Y') {
+      console.log('Shift + Y is pressed', event);
+    }
+  }
+  
 }
