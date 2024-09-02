d74767fc58943ea4b6dcfe7238c650981d69215e
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2024-09-02 06:16:19 +0530

    ngSwitch and Attribute Directives

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -8,25 +8,42 @@ import { Component } from '@angular/core';
 export class DirectivesComponent {
   display: boolean = true;
   num: number = 0;
-  displayMsg : boolean = true;
-
+  displayMsg: boolean = true;
+  age: number = 25;
+  ageVal: number = 0;
+  isEligible: boolean = false;
   onBtnClick() {
     this.displayMsg = !this.displayMsg;
   }
 
-  showStack : boolean = false;
+  showStack: boolean = false;
   showStackFunc() {
     this.showStack = !this.showStack;
   }
 
   frontEnd = [
     {
-      id : 1,
-      name : 'Angular'
+      id: 1,
+      name: 'Angular'
     },
     {
-      id : 2,
-      name : 'React'
+      id: 2,
+      name: 'React'
     }
   ]
+
+  checkEligibility() {
+    // if (this.ageVal >= 18) {
+    //   this.isEligible = true;
+    // }
+    // else {
+    //   this.isEligible = false
+    // }
+
+    this.isEligible = this.ageVal >= 18;
+  }
+
+  // -------------------------------> Attribute Directives <-------------------------------
+
+  // updateStyles = 'updateStyles'
 }
