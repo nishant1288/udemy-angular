d399992d4e5c878c32a3ae0bf8953ffeff254a11
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2024-09-28 06:20:17 +0530

    ngOnChanges lifecycle method

--- src/app/component-lifecycle/component-lifecycle.component.ts
+++ src/app/component-lifecycle/component-lifecycle.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-component-lifecycle',
   templateUrl: './component-lifecycle.component.html',
   styleUrls: ['./component-lifecycle.component.css']
 })
-export class ComponentLifecycleComponent {
+export class ComponentLifecycleComponent implements OnChanges{
+
+  @Input() inputValue : string = ''
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges triggered');
+    console.log(changes);
+  }
 
 }
