f8975d10a7c15d59ae1a41361e7eb056c433d268
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2024-09-29 07:12:54 +0530

    ngOnInIt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myfirstProject';
 
-  inputValue : string ='hello';
+  inputValue: string = 'hello';
 
-  inputPropValue : string = 'input value';
+  inputPropValue: string = 'input value';
 
   update() {
     this.inputPropValue = 'input value 2'
   }
-  
+
+  counte: number = 0;
+
+  ngOnInit(): void {
+    this.updateCounter();
+  }
+
+  updateCounter() {
+    setInterval(() => {
+      if (this.counte < 5) {
+        this.counte++;
+        console.log(this.counte);
+      }
+    }, 1000);
+  }
+
 }
